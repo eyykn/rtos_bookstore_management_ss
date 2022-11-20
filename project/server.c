5496ca63367da9abec0945ef47beb4c01fd88d73
mk-klj <80366423+mk-klj@users.noreply.github.com>
2022-11-19 19:40:39 -0500

    Update server.c

--- project/server.c
+++ project/server.c
@@ -1,3 +1,64 @@
-int main(int argc, char **argv)
-{
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/neutrino.h>
+#include <process.h>
+#include <string.h>
+#include <sys/iofunc.h>
+#include <sys/dispatch.h>
+
+#include "server.h" // defines messages between client & server
+
+int main(int argc, char **argv){
+
+	name_attach_t *attach;
+	int rcvid;
+	struct _msg_info info;
+	//Create struct for msg + include in server.h
+
+	attach=name_attach(NULL, SERVER_NAME, 0);
+
+	if (attach==NULL){
+		printf("Name attach failed.\n");
+		return EXIT_FAILURE;
+	}
+
+	while (1) {
+			//Receives message
+			rcvid=MsgReceive(attach->chid, &msg, sizeof(msg), &info);
+			 if(rcvid==0) {
+				//Pulse
+				 switch (msg.pulse.code) {
+				 case _PULSE_CODE_DISCONNECT:
+					 printf("Received disconnect pulse\n");
+					 if (-1 == ConnectDetach(msg.pulse.scoid)) {
+						 perror("ConnectDetach");
+				 	 }
+				 	break;
+				 case _PULSE_CODE_UNBLOCK:
+				 	printf("Received unblock pulse\n");
+				 	if (-1 == MsgError(msg.pulse.value.sival_int, -1)) {
+				 		perror("MsgError");
+				 	}
+				 	break;
+
+				 default:
+				 	printf("unknown pulse received, code = %d\n", msg.pulse.code);
+
+				 }
+			} else {
+				//Message
+				if (rcvid == -1) {
+					perror("MsgReceive");
+					exit(EXIT_FAILURE);
+
+				} else if (rcvid > 0) {
+					//Add Cases
+				}
+
+			}
+		}
 }
