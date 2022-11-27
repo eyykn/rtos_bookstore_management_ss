802a8e337678493e29dc7c298d5adf582f632076
e-yay <61576902+e-yay@users.noreply.github.com>
2022-11-26 20:03:25 -0500

    trial

--- project/client.c
+++ project/client.c
@@ -13,7 +13,6 @@
 #define NUMTHREADS      3
 
 int printMenu();
-int getDigitCount();
 void* getClientOrder();
 
 int main(int argc, char **argv) {
@@ -105,53 +104,59 @@ void* getClientOrder(int coid){
 		// predetermined to only let a client order 2 books in one session
 		do {
 			printf("Enter book number to order as X, order date as DD/MM/YY, class start as DD/MM/YY, class start time as HH:MM ->\n");
+			char* inputs = malloc(17 * sizeof(char));
 			int orderNum, odDay, odMon, odYr, cdDay, cdMon, cdYr, ctHr, ctMin;
 			//for later: always reads in 4 (after enter it doesn't count how many fields??)
-			if(scanf("%d %d/%d/%d %d/%d/%d %d:%d", &orderNum, &odDay, &odMon, &odYr, &cdDay, &cdMon, &cdYr, &ctHr, &ctMin)!=9){
+			printf("PRE scanned inputs %s.\n", inputs);
+			int scanned = scanf("%c %c%c/%c%c/%c%c %c%c/%c%c/%c%c %c%c:%c%c", &inputs[0], &inputs[1], &inputs[2], &inputs[3], &inputs[4], &inputs[5], &inputs[6], &inputs[7], &inputs[8], &inputs[9], &inputs[10], &inputs[11], &inputs[12], &inputs[13], &inputs[14], &inputs[15], &inputs[16]);
+			if (scanned!=17) {
 				printf("Please enter all fields to complete your order.\n");
-				printf("order num digits= %d\n", getDigitCount(orderNum));
-				printf("order date digits= %d|%d|%d\n", getDigitCount(odDay), getDigitCount(odMon), getDigitCount(odYr));
-				printf("class date digits= %d|%d|%d\n", getDigitCount(cdDay), getDigitCount(cdMon), getDigitCount(cdYr));
-				printf("class time digits= %d|%d\n", getDigitCount(ctHr), getDigitCount(ctMin));
+				printf("Scanned %d.\n", scanned);
+				printf("Scanned inputs %s.\n", inputs);
 				fflush(stdin);
-			} else if (getDigitCount(orderNum) !=1 ||orderNum > BOOK_COUNT || orderNum < 1) {
-				printf("Please ensure book entered is in the menu presented & is in format X (1 digit). \n");
-			} else if ((getDigitCount(odDay) + getDigitCount(odMon) + getDigitCount(odYr)) !=6) {
-				printf("Please enter order date as DD/MM/YY (6 digits).\n");
-			} else if ((getDigitCount(cdDay) + getDigitCount(cdMon) + getDigitCount(cdYr))!=6) {
-				printf("Please enter class date as DD/MM/YY (6 digits).\n");
-			} else if ((getDigitCount(ctHr) + getDigitCount(ctMin)) !=4) {
-				printf("Please enter class time as HH:MM (4 digits).\n");
-			} else if (odDay<0 || odDay>31 || odMon <0 || odMon>12 || odYr<0 || odYr>22 || cdDay<0||
+			} else {
+				printf("in else: Scanned %d.\n", scanned);
+				printf("in else: Scanned inputs %s.\n", inputs);
+				orderNum = atoi(&inputs[0]);
+				odDay = (10 * (inputs[1] - '0' ) ) + inputs[2] - '0';
+				odMon = (10 * (inputs[3] - '0' ) ) + inputs[4] - '0';
+				odYr = (10 * (inputs[5] - '0' ) ) + inputs[6] - '0';
+				cdDay = (10 * (inputs[7] - '0' ) ) + inputs[8] - '0';
+				cdMon = (10 * (inputs[9] - '0' ) ) + inputs[10] - '0';
+				cdYr = (10 * (inputs[11] - '0' ) ) + inputs[12] - '0';
+				ctHr = (10 * (inputs[13] - '0' ) ) + inputs[14] - '0';
+				ctMin = (10 * (inputs[15] - '0' ) ) + inputs[16] - '0';
+				if (odDay<0 || odDay>31 || odMon <0 || odMon>12 || odYr<0 || odYr>22 || cdDay<0||
 					cdDay>31 || cdMon<0|| cdMon>12 || cdYr<0 || cdYr>22 || ctHr<00 || ctHr>24|| ctMin<00|| ctMin>59){
-				printf("Please ensure that date/time values are valid.\n");
-			}else if(odMon==04 || odMon==06 || odMon==9 || odMon==11){
-				if(odDay>30){
-					printf("Please ensure that the day value of Order Month is valid.\n");
+					printf("Please ensure that date/time values are valid.\n");
+				}else if(odMon==04 || odMon==06 || odMon==9 || odMon==11){
+					if(odDay>30){
+						printf("Please ensure that the day value of Order Month is valid.\n");
+					}
+				}else if(cdMon==04 || cdMon==06 || cdMon==9 || cdMon==11){
+					if(cdDay>30){
+						printf("Please ensure that the day value of Class Month is valid.\n");
+					}
+				}else if(cdMon==02 || odMon==02){
+					if(cdDay>28 || odDay>28){
+						printf("Please ensure that the date values (DD/MM/YY) are valid.");
+					}
+				}else{
+					// correct input path
+					orderNums[bookCount] = orderNum;
+					orderDateDay[bookCount] = odDay;
+					orderDateMon[bookCount] = odMon;
+					orderDateYr[bookCount] = odYr;
+					classDateDay[bookCount] = cdDay;
+					classDateMon[bookCount] = cdMon;
+					classDateYr[bookCount] = cdYr;
+					classTimeHr[bookCount] = ctHr;
+					classTimeMin[bookCount] = ctMin;
+					fflush(stdin);
+					bookCount++;
 				}
-			}else if(cdMon==04 || cdMon==06 || cdMon==9 || cdMon==11){
-				if(cdDay>30){
-					printf("Please ensure that the day value of Class Month is valid.\n");
-				}
-			}else if(cdMon==02 || odMon==02){
-				if(cdDay>28 || odDay>28){
-					printf("Please ensure that the date values (DD/MM/YY) are valid.");
-				}
-			}else{
-				// correct input path
-				orderNums[bookCount] = orderNum;
-				orderDateDay[bookCount] = odDay;
-				orderDateMon[bookCount] = odMon;
-				orderDateYr[bookCount] = odYr;
-				classDateDay[bookCount] = cdDay;
-				classDateMon[bookCount] = cdMon;
-				classDateYr[bookCount] = cdYr;
-				classTimeHr[bookCount] = ctHr;
-				classTimeMin[bookCount] = ctMin;
-				fflush(stdin);
-				bookCount++;
 			}
-
+			memset(inputs, 0, 17); // resets array
 		} while (bookCount < MAX_BOOKS);
 
 		// build message
@@ -170,6 +175,7 @@ void* getClientOrder(int coid){
 		}
 
 		// copy paste for sample input: 1 12/12/12 12/12/12 11:11
+		// copy paste for sample input w/ zeros: 1 02/02/02 02/02/02 01:01
 		printf("order info for 1st order: %d %d %d %d %d %d %d %d %d\n", send_order_msg.orderInfo[0][0], send_order_msg.orderInfo[0][1], send_order_msg.orderInfo[0][2], send_order_msg.orderInfo[0][3], send_order_msg.orderInfo[0][4], send_order_msg.orderInfo[0][5], send_order_msg.orderInfo[0][6], send_order_msg.orderInfo[0][7], send_order_msg.orderInfo[0][8]);
 		printf("order info for 2nd order: %d %d %d %d %d %d %d %d %d\n", send_order_msg.orderInfo[1][0], send_order_msg.orderInfo[1][1], send_order_msg.orderInfo[1][2], send_order_msg.orderInfo[1][3], send_order_msg.orderInfo[1][4], send_order_msg.orderInfo[1][5], send_order_msg.orderInfo[1][6], send_order_msg.orderInfo[1][7], send_order_msg.orderInfo[1][8]);
 
@@ -183,16 +189,6 @@ void* getClientOrder(int coid){
 
 
 		return (NULL);
-}
-
-
-int getDigitCount(int num) {
-	int digits=0;
-	while(num!=0) {
-		num=num/10;
-		digits++;
-	}
-	return digits;
 }
 
 
